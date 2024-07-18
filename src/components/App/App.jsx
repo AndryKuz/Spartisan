6ed671a3db59599dffb44a7df8fdd0fe5a51cf79
Andry Kuz <femon1992@gmail.com>
2024-07-18 23:56:11 +0300

    main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import MainPage from "../MainPage/MainPage";
 import AppRoutes from "../Routes/AppRoutes";
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
+      <MainPage/>
       <main>
         <div className="container">
           <AppRoutes />
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+import style from "./MainPage.module.scss";
+import { ReactComponent as Arrow } from "../../assets/images/ExpandDown.svg";
+
+const MainPage = () => {
+    return (
+        <section className={style.pageParent}>
+            <div className={style.mainPage}>
+                <h3>The art of restoring the body through</h3>
+                <h2>bath procedures</h2>
+                <div>
+                    <span></span>
+                </div>
+                <Arrow />
+            </div>
+        </section>
+    );
+};
+
+export default MainPage;
